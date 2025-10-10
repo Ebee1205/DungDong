8df2c7c5d1be1bcf468c355fec8ada34bb2da648
Ebee1205 <spiverain02@gmail.com>
2025-10-11 01:55:19 +0900

    [feat] home 화면 UI수정

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -14,7 +14,7 @@
     </v-row>
 
     <v-card
-      class="mx-auto | margin-top-72 | pt-2 pl-4 pr-4"
+      class="mx-auto | margin-top-56 | pt-2 pl-4 pr-4"
       max-width="320" rounded="lg"
     >
       <v-card-title class="card-title-text">
@@ -35,8 +35,8 @@
     <v-row 
       no-gutters justify="center" class="margin-42 | mt-12"
     >
-      <v-col class="pa-0 text-center">
-        <div class="speech-bubble float-animation">
+      <v-col class="pa-0 | text-center">
+        <div class="speech-bubble | float-animation">
           👇 이미 작성한 글이 있다면 👇
         </div>
         <v-btn 
@@ -48,20 +48,21 @@
         </v-btn>
       </v-col>
     </v-row>
-    <div style="font-size: 16px; margin: 10px auto; text-align: center;">혹은</div>
+
+    <div style="font-size: 15px; margin: 10px auto; text-align: center;">혹은</div>
+    
     <v-row 
-      v-if="sNewStart"
       no-gutters justify="center" class="margin-42 | mt-2"
     >
       <v-btn 
         @click="handleClickStartBtn"
-        variant="flat" color="#FF6161" rounded="xl" size="large" width="100%" height="52px"
-        class="text-btn"
+        variant="flat" color="#FF6161" rounded="xl" size="large" width="80%" height="32px"
+        class="text-small-btn"
       >
         직접 체크하여 제작
       </v-btn>
     </v-row>
-    <v-row 
+    <!-- <v-row 
       v-else="sNewStart"
       no-gutters justify="center" class="margin-42 | mt-12"
     >
@@ -72,7 +73,7 @@
       >
         다시 시작
       </v-btn>
-    </v-row>
+    </v-row> -->
     <!-- <v-row 
       v-if="sCtnStart"
       no-gutters justify="center" class="margin-42 | mt-2"
@@ -167,8 +168,8 @@ function handleClickContBtn() {
   margin-right: 42px;
 }
 
-.margin-top-72 {
-  margin-top: 72px;
+.margin-top-56 {
+  margin-top: 56px;
 }
 
 .margin-88 {
@@ -192,7 +193,15 @@ function handleClickContBtn() {
   font-style: normal;
   font-weight: 700;
   letter-spacing: -0.4px;
-  transition: transform 0.2s ease-in-out;
+}
+
+.text-small-btn {
+  color: #FFF;
+  text-align: center;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  letter-spacing: -0.4px;
 }
 
 .text-btn:hover {
@@ -213,15 +222,15 @@ function handleClickContBtn() {
   position: relative;
   background-color: #FFFFFF;
   color: #000000;
-  border: 1px solid #424242;
+  border: 0.5px solid #C0C0C0;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   padding: 8px 16px;
   border-radius: 10px;
   display: inline-block;
   margin-bottom: 22px; /* Increased margin for tail */
-  font-size: 14px;
-  font-weight: bold;
-  filter: drop-shadow(0 3px 2px rgba(0, 0, 0, 0.2));
+  font-size: 13px;
+  font-weight: 500;
+  filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.2));
 }
 
 /* Tail border */
@@ -235,7 +244,7 @@ function handleClickContBtn() {
   height: 0;
   border-left: 10px solid transparent;
   border-right: 10px solid transparent;
-  border-top: 10px solid #424242;
+  border-top: 10px solid #C0C0C0;
 }
 
 /* Tail fill */
