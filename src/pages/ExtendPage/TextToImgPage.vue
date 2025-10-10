24c1e8e66de9eff4637727cff2474f0e6446f508
Ebee1205 <spiverain02@gmail.com>
2025-10-11 01:27:54 +0900

    [feat] template에 맞도록 리팩토링, 텍스트 수정

--- src/pages/ExtendPage/TextToImgPage.vue
+++ src/pages/ExtendPage/TextToImgPage.vue
@@ -1,47 +1,74 @@
 <template>
-  <v-container>
-    <v-row no-gutters justify="center" class="mt-2">
-      <v-col class="subtitle-text">
-        룸메이트 구인 글을 이곳에 입력하여 둥지동지 결과이미지를 제작해보세요.
-      </v-col>
-    </v-row>
-    <v-row no-gutters justify="center" class="mt-8">
-      <v-col cols="12" md="12" lg="12">
+  <BoxContainer>
+    <SubTitle :title="title" :desc="desc"> </SubTitle>
+
+    <v-row no-gutters justify="start" class="mt-4">
+      <v-col>
         <v-textarea
+          class="text-area-custom"
           label="룸메이트 구인 글을 입력해주세요."
           variant="outlined"
+          v-model="surveytext"
           :rows="textareaRows"
-          auto-grow
           max-rows="18"
-          class="text-area-custom"
+          auto-grow
           @focus="onFocus"
           @blur="onBlur"
         ></v-textarea>
       </v-col>
     </v-row>
-  </v-container>
+
+  </BoxContainer>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
-import { useRouter } from 'vue-router';
 
-const router = useRouter();
+import { onMounted, onUnmounted, ref, computed, watch} from "vue";
+import { useRouter, useRoute } from "vue-router";
+import { routes } from "@/router"
+import BoxContainer from "@/components/BoxContainer.vue";
+
+const title = '구인글로 이미지 생성하기'
+const desc = '이미 작성한 룸메이트 구인글이 있다면, 붙여넣기하여 간편하게 둥지동지 이미지로 변환해보세요.'
+
 const isMobile = ref(false);
 const textareaRows = ref(15);
+const surveytext = ref(null);
 
-const checkMobile = () => {
-  isMobile.value = window.innerWidth < 600;
-};
-
+// ----- 라이프 사이클 ----- //
 onMounted(() => {
-  checkMobile();
-  window.addEventListener('resize', checkMobile);
+  setCurrentSurvey()
 });
 
 onUnmounted(() => {
-  window.removeEventListener('resize', checkMobile);
 });
+
+// ----- 함수 정의 ----- //
+
+// 최초 로딩
+function setCurrentSurvey() {
+  localStorage.setItem('userProgress', JSON.stringify({ currentStep: 11}));
+  console.log("Updated localStorage userProgress:", localStorage.getItem('userProgress'))
+
+  // 저장된 값이 있다면 가져오기
+  const existingSurvey = localStorage.getItem('userSurvey');
+  
+  if (existingSurvey) {
+    const survey = JSON.parse(existingSurvey);
+  }
+}
+
+// 변경값 로컬스토리지에 저장
+function updateLocalStorage(field, value) {
+  const existingSurvey = JSON.parse(localStorage.getItem("userSurvey")) || {};
+  existingSurvey[field] = value;
+  localStorage.setItem("userSurvey", JSON.stringify(existingSurvey));
+  console.log(`Updated localStorage userSurvey: ${field} = ${value}`);
+}
+
+function onBlur() {
+  textareaRows.value = 15; 
+}
 
 function onFocus() {
   if (isMobile.value) {
@@ -49,16 +76,6 @@ function onFocus() {
   }
 }
 
-function onBlur() {
-  textareaRows.value = 15; 
-}
-
-function finish() {
-  // 기능 구현은 나중에 할게요..
-  console.log('Finish button clicked');
-  // 일단 홈으로 돌려보냅니다.
-  router.push('/home');
-}
 </script>
 
 <style scoped>
